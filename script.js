a5d03b2642b6fb7ac8a31c2d3bfcc31e23ae1a2b
Sascha-La <al.artishevskaya98@gmail.com>
2024-10-06 21:32:28 +0300

    add 1st part of code
    Почему-то все время возвращает 1, но не могу пока понять, почему так. Вероятно где-то ошибка в коде.

--- script.js
+++ script.js
@@ -2,6 +2,36 @@
 
 /* Заранее заготовь 3-5 предсказаний и в зависимости от того, как лягут карты судьбы (или что скажет Math.random) показывай их пользователю */
 
+const prediction1 = "Скоро вы отправитесь в поездку.";
+const prediction2 = "Сегодня чаще смотри по сторонам, рядом кое-что интересное!";
+const prediction3 = "Берегите людей, после встречи с которыми, что-то светлое и радостное поселяется в вашей душе";
+const prediction4 = "Осторожно, впереди волна впечатлений!";
+const prediction5 = "Кушай сладости - это к радости!";
+
+const forecast = document.querySelector('.current-forecast');
+const button = document.querySelector('.forecast-btn');
+
+button.addEventListener('click', function(event) {
+
+    if (getRandomInt = 1) {
+        forecast.append(prediction1);
+    } else if (getRandomInt = 2) {
+        forecast.append(prediction2);
+    } else if (getRandomInt = 3) {
+        forecast.append(prediction3);
+    } else if (getRandomInt = 4) {
+        forecast.append(prediction4);
+    } else if (getRandomInt = 5) {
+        forecast.append(prediction5);
+    }
+});
+
+function getRandomInt(min, max) {
+    return MathFloor(Math.random() * (max - min)) + min;
+};
+
+console.log(getRandomInt(1, 6));
+
 /* Подставляй текст нового предсказания в .current-forecast h1 */
 
 /* Показывай процент вероятности, с которым предсказание сбудется — в верстке это .current-forecast p */
