eb45b3459c3dac4cc89ea1dadd0c5ebd20d74157
Sascha-La <al.artishevskaya98@gmail.com>
2024-10-27 21:57:03 +0300

    Update JS

--- script.js
+++ script.js
@@ -2,36 +2,6 @@
 
 /* Заранее заготовь 3-5 предсказаний и в зависимости от того, как лягут карты судьбы (или что скажет Math.random) показывай их пользователю */
 
-const prediction1 = "Скоро вы отправитесь в поездку.";
-const prediction2 = "Сегодня чаще смотри по сторонам, рядом кое-что интересное!";
-const prediction3 = "Берегите людей, после встречи с которыми, что-то светлое и радостное поселяется в вашей душе";
-const prediction4 = "Осторожно, впереди волна впечатлений!";
-const prediction5 = "Кушай сладости - это к радости!";
-
-const forecast = document.querySelector('.current-forecast');
-const button = document.querySelector('.forecast-btn');
-
-button.addEventListener('click', function(event) {
-
-    if (getRandomInt = 1) {
-        forecast.append(prediction1);
-    } else if (getRandomInt = 2) {
-        forecast.append(prediction2);
-    } else if (getRandomInt = 3) {
-        forecast.append(prediction3);
-    } else if (getRandomInt = 4) {
-        forecast.append(prediction4);
-    } else if (getRandomInt = 5) {
-        forecast.append(prediction5);
-    }
-});
-
-function getRandomInt(min, max) {
-    return MathFloor(Math.random() * (max - min)) + min;
-};
-
-console.log(getRandomInt(1, 6));
-
 /* Подставляй текст нового предсказания в .current-forecast h1 */
 
 /* Показывай процент вероятности, с которым предсказание сбудется — в верстке это .current-forecast p */
@@ -43,3 +13,50 @@ console.log(getRandomInt(1, 6));
 /* При генерации нового предсказания старое предсказание должно добавляться в начало списка «Мои предсказания» — .forecasts  */
 
 /* Для добавления предсказания в список воспользуйся шаблоном forecast-item */
+
+const forecast = document.querySelector('.current-forecast');
+const textOfForecast = document.querySelector('.current-forecast h1');
+const textOfProbability = document.querySelector('.current-forecast p');
+const button = document.querySelector('.forecast-btn');
+const element = document.querySelector('#forecast-item');
+const savedPredictions = document.querySelector('.forecasts');
+
+function getRandomInt(min, max) {
+    return MathFloor(Math.random() * (max - min)) + min;
+};
+const randomNumber = getRandomInt(1, 6);
+
+let predictionText = '';
+const probability = getRandomInt(0, 101);
+
+button.addEventListener('click', function(event) {
+
+    if (randomNumber == 1) {
+        predictionText = "Скоро вы отправитесь в поездку.";
+        textOfProbability.textContent = probability;
+    } else if (randomNumber == 2) {
+        predictionText = "Сегодня чаще смотри по сторонам, рядом кое-что интересное!";
+        textOfProbability.textContent = probability;
+    } else if (randomNumber == 3) {
+        predictionText = "Берегите людей, после встречи с которыми, что-то светлое и радостное поселяется в вашей душе";
+        textOfProbability.textContent = probability;
+    } else if (randomNumber == 4) {
+        predictionText = "Осторожно, впереди волна впечатлений!";
+        textOfProbability.textContent = probability;
+    } else(randomNumber == 5) {
+        predictionText = "Кушай сладости - это к радости!";
+        textOfProbability.textContent = probability;
+    }
+});
+
+function template(predictionText, probability) {
+    const newElement = element.content.cloneNode(true);
+    newElement.querySelector('h3').textContent = predictionText;
+    newElement.querySelector('p').textContent = probability;
+
+    return newElement;
+
+};
+
+const listItem = template();
+savedPredictions.prepend(listItem);
